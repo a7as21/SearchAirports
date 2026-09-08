760b0dcb8702fcc47211927034b7c69177b8b4cd
a7as21 <ahmed.alsuwaidi2492@gmail.com>
2026-09-08 20:37:29 +0400

    fixed search for airports

--- search.js
+++ search.js
@@ -11,27 +11,49 @@ function findAirport(entry) {
     entry = entry.toLowerCase().trim()
     const results = []
 
-    for(const airport of airports) {
-       
-        const iata = airport.iata_code || "";
-        const name = airport.name || "";
-        const municipality = airport.municipality || "";
-        const iso_country = airport.iso_country || "";
+    if (entry.length == 2) {
 
-        if(airport.iata_code !== "") {
-            if(
-                iata.toLowerCase().includes(entry) || 
-                name.toLowerCase().includes(entry) ||
-                municipality.toLowerCase().includes(entry) || 
-                iso_country.toLowerCase().includes(entry)  
-            ) {
-                results.push([iata, municipality, name, iso_country]);
-            } 
+         for(const airport of airports) {
+       
+            const iata = airport.iata_code || "";
+            const name = airport.name || "";
+            const municipality = airport.municipality || "";
+            const iso_country = airport.iso_country || "";
+
+            if(airport.iata_code !== "") {
+                if(iso_country.toLowerCase().includes(entry)) {
+                    results.push([iata, municipality, name, iso_country]);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    else {
+        for(const airport of airports) {
+       
+            const iata = airport.iata_code || "";
+            const name = airport.name || "";
+            const municipality = airport.municipality || "";
+            const iso_country = airport.iso_country || "";
+
+            if(airport.iata_code !== "") {
+                if(
+                    iata.toLowerCase().includes(entry) || 
+                    name.toLowerCase().includes(entry) ||
+                    municipality.toLowerCase().includes(entry) || 
+                    iso_country.toLowerCase().includes(entry)  
+                ) {
+                    results.push([iata, municipality, name, iso_country]);
+                } 
+            }
         }
     }
 
     return results;
 }
+
 
 document.getElementById("searchButton").addEventListener("click", () => {
 
